eb78632a16af413bf54a0690648152522ce979b4
Thiago Medeiros <medeiroshs@hotmail.com>
2022-06-30 13:23:20 -0300

    feat: colocando a rota http /:id para getProduct

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -4,5 +4,6 @@ const productsController = require('../controllers/productsController');
 const productRoute = Router();
 
 productRoute.get('/', productsController.listProducts);
+productRoute.get('/:id', productsController.getProduct);
 
 module.exports = productRoute;
