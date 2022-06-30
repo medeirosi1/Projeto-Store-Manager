50a2e21e75d39784eaf98c6e5679bf6cb6c4816b
Thiago Medeiros <medeiroshs@hotmail.com>
2022-06-30 13:25:11 -0300

    feat: fazendo a validação em caso de erro de um id não achado no banco de dados e se caso der certo passar o objeto encotrado

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,9 +1,19 @@
 const productsService = require('../services/productsService');
+const productsModel = require('../models/productsModel');
 
 const productsController = {
   async listProducts(_req, res) {
     const items = await productsService.listProducuts();
     res.status(200).json(items);
+  },
+
+  async getProduct(req, res) {
+    const { id } = req.params;
+    if (!await productsModel.existsId(id)) { 
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    const item = await productsService.getProduct(id);
+    res.status(200).json(item);
   },
 };
 
