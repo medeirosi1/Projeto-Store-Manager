46d9e2fdaee18b7afd239884e64153d8cfd29db7
Thiago Medeiros <medeiroshs@hotmail.com>
2022-06-30 13:22:58 -0300

    feat: criando uma query para verificar se existe o id específico e criando uma função chamado getProduct para pegar o id especifico passado

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,10 +2,26 @@ const connection = require('./connection');
 
 const productsModel = {
   async listProducuts() {
-  const query = 'SELECT * FROM StoreManager.products';
-  const [items] = await connection.execute(query);
-  return items;
-},
+    const query = 'SELECT * FROM StoreManager.products';
+    const [items] = await connection.execute(query);
+    return items;
+  },
+  
+  async getProduct(id) {
+    const query = 'SELECT * FROM StoreManager.products WHERE id = ? ';
+    const [[item]] = await connection.execute(query, [id]);
+    return item;
+  },
+
+  async existsId(id) {
+    const query = `
+      SELECT 1
+      FROM StoreManager.products 
+      WHERE id = ?
+    `;
+    const [[exists]] = await connection.execute(query, [id]);
+    return !!exists;
+  },
 
 };
 module.exports = productsModel;
